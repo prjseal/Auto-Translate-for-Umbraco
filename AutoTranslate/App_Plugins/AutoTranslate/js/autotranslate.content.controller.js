7c964778ea89da003ae4032e8b0f2aff5e5e8950
Paul Seal <prjseal@gmail.com>
2019-05-30 17:14:12 +0100

    - added some toggles for options on the content auto translate menu

--- AutoTranslate/App_Plugins/AutoTranslate/js/autotranslate.content.controller.js
+++ AutoTranslate/App_Plugins/AutoTranslate/js/autotranslate.content.controller.js
@@ -1,8 +1,11 @@
 (function () {
     'use strict';
 
-    function AutoTranslateContent($scope, $http, editorState, $routeParams, $window) {
+    function AutoTranslateContent($scope, $http, editorState, $routeParams, $window, navigationService) {
         var apiUrl;
+        var vm = this;
+        vm.includeDescendants = false;
+        vm.overwriteExisting = false;
 
         function init() {
             var culture = $routeParams.cculture ? $routeParams.cculture : $routeParams.mculture;
@@ -11,23 +14,49 @@
 
         $scope.getTranslatedText = function () {
             var culture = $routeParams.cculture ? $routeParams.cculture : $routeParams.mculture;
-
+            vm.loading = true;
             $http({
                 method: 'POST',
                 url: apiUrl + 'GetTranslatedText/',
                 data: JSON.stringify({
                     CurrentCulture: culture,
                     NodeId: editorState.current.id,
-                    OverwriteExistingValues: true,
-                    IncludeDescendants: true
+                    OverwriteExistingValues: vm.overwriteExisting,
+                    IncludeDescendants: vm.includeDescendants
                 }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             }).then(function (response) {
+                vm.loading = false;
                 $window.location.reload(true);
             });
         };
+
+        $scope.closeDialog = function () {
+            navigationService.hideDialog();
+        };
+
+        $scope.toggle = toggleHandler;
+
+        function toggleHandler(type) {
+            // If the recurvise toggle is clicked
+            if (type === "recursive") {
+                if (vm.includeDescendants) {
+                    vm.includeDescendants = false;
+                    return;
+                }
+                vm.includeDescendants = true;
+            }
+
+            if (type === "overwrite") {
+                if (vm.overwriteExisting) {
+                    vm.overwriteExisting = false;
+                    return;
+                }
+                vm.overwriteExisting = true;
+            }
+        }
 
         init();
     }
--- AutoTranslate/App_Plugins/AutoTranslate/js/autotranslate.dictionary.controller.js
+++ AutoTranslate/App_Plugins/AutoTranslate/js/autotranslate.dictionary.controller.js
@@ -1,8 +1,9 @@
 (function () {
     'use strict';
 
-    function AutoTranslateDictionary($scope, $http, editorState, $routeParams, $window) {
+    function AutoTranslateDictionary($scope, $http, editorState, $routeParams, $window, navigationService) {
         var apiUrl;
+        var vm = this;
 
         function init() {
             var culture = $routeParams.cculture ? $routeParams.cculture : $routeParams.mculture;
@@ -11,7 +12,7 @@
 
         $scope.getTranslatedText = function () {
             var culture = $routeParams.cculture ? $routeParams.cculture : $routeParams.mculture;
-
+            vm.loading = true;
             $http({
                 method: 'POST',
                 url: apiUrl + 'TranslateDictionaryItems/',
@@ -26,8 +27,13 @@
                     'Content-Type': 'application/json'
                 }
             }).then(function (response) {
+                vm.loading = false;
                 $window.location.reload(true);
             });
+        };
+
+        $scope.closeDialog = function () {
+            navigationService.hideDialog();
         };
 
         init();
